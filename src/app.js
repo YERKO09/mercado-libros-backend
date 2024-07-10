43b90ce18de72053c432a5b51b87ecc6ecd5abd0
Raul Espinoza M <raul.espmol@gmail.com>
2024-07-09 20:10:23 -0400

    agregar variables de entorno
    Agrego DB_PORT en la configuracion de DB, y FRONT_URL para el cors en app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
+require('dotenv').config();
 const express = require('express')
 const routes = require('./routes/index');
 const cors = require('cors')
 const morgan = require('morgan');
 const { handleErrors } = require('./middlewares/errorsHandler');
+const { FRONT_URL } = process.env
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors({
+  origin: FRONT_URL
+}))
 
 // routes
 app.use('/api', routes)
